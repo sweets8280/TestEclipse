fc8b014c73f4166b6819f8dff22c743cfcbd3650
yuuto8280 <phillip.momo1119@gmail.com>
2021-09-06 14:18:45 +0900

    コミット

--- src/test/GitTest.java
+++ src/test/GitTest.java
@@ -5,7 +5,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("HelloGitHob!");
 		System.out.println("GitHobに接続成功！");
-		System.out.println("トークン接続になっていた！");
+		System.out.println("トークン接続になっていた!");
+		System.out.println("aaa");
 	}
 
 }
